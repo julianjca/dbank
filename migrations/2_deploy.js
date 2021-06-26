67fc27e60cd6aeaef2b127f0e43810f9bc498c25
Julian Anderson <juliancanderson@gmail.com>
2021-06-26 20:28:24 +0700

    feat: initial migrations

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -4,8 +4,10 @@ const dBank = artifacts.require("dBank");
 module.exports = async function(deployer) {
 	//deploy Token
 
+  await deployer.deploy(Token)
+
 	//assign token into variable to get it's address
-	
+
 	//pass token address for dBank contract(for future minting)
 
 	//assign dBank contract into variable to get it's address
